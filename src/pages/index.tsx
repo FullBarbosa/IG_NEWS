4fe5b0ff0eb722b25f7642a3271ed2dbce875560
FullBarbosa <diibarbosa22@gmail.com>
2021-08-11 22:58:53 -0300

    post jest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
-import { SubscribeButton } from '../components/SubscribeButton';
-import { stripe } from '../services/stripe';
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
-import styles from './home.module.scss';
+import styles from "./home.module.scss";
 
-interface HomeProps{
-  product:{
+interface HomeProps {
+  product: {
     priceId: string;
-    amount: number;
-  }
+    amount: string;
+  };
 }
 
-export default function Home({ product } : HomeProps) {
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -22,25 +22,15 @@ export default function Home({ product } : HomeProps) {
         <section className={styles.hero}>
           <span>👏 hey, welcome</span>
           <h1>
-            News about tje
-            {' '}
-            <span>React</span>
-            {' '}
-            World.
+            News about tje <span>React</span> World.
           </h1>
           <p>
             Get access to all the publications
             <br />
-            <span>
-              for
-              {' '}
-              {product.amount}
-              {' '}
-              month
-            </span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="" />
@@ -49,17 +39,17 @@ export default function Home({ product } : HomeProps) {
   );
 }
 
-export const getStaticProps:GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1IaknFGe2ssae53nILk7uysN', {
-    expand: ['product'],
-  });// buscando dados do componente stripe.ts
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1IaknFGe2ssae53nILk7uysN", {
+    expand: ["product"],
+  }); // buscando dados do componente stripe.ts
 
   // tratando dados da api do stripe
   const product = {
     priceId: price.id,
-    amount: new Intl.NumberFormat('en-us', {
-      style: 'currency',
-      currency: 'USD',
+    amount: new Intl.NumberFormat("en-us", {
+      style: "currency",
+      currency: "USD",
     }).format(price.unit_amount / 100), // formatando valor de dinheiro
   };
 
